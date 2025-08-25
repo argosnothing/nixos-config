d662c984b4bf31a72c26d8839821d471359eae66
argos <argosnothing@gmail.com>
2025-08-24 20:33:41 -0400

    restructiuring

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,8 +1,8 @@
 # Tie All the hosts together
 {inputs, pkgs, pkgsUnstable, system, ...}: let
-  desktop = import ./desktop/default.nix {inherit inputs pkgs pkgsUnstable system settings;};
-  laptop = import ./laptop/default.nix {inherit inputs pkgs pkgsUnstable system settings;};
-  settings = {
+  desktop = import ./desktop/default.nix {inherit inputs pkgs pkgsUnstable system defaultSettings;};
+  laptop = import ./laptop/default.nix {inherit inputs pkgs pkgsUnstable system defaultSettings;};
+  defaultSettings = {
     username = "salivala";
     wm = "hyprland";
     monoFont = "FiraCode Nerd Font";
@@ -11,7 +11,6 @@
     serifFontPkg = pkgs.liberation_ttf;
     sansFont = "Liberation Sans";
     sansFontPkg = pkgs.liberation_ttf;
-    # Stylix theme
     stylixTheme = "gruvbox-dark-hard";
   };
 in {
--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -1,18 +1,29 @@
 # Configure both system and user for this machine
-{inputs, pkgs, pkgsUnstable, system, settings, ...}:
 {
-    nixosConfigurations = {
-      desktop = inputs.nixpkgs.lib.nixosSystem {
-        inherit system;
-        specialArgs = { inherit inputs settings pkgsUnstable; };
-        modules = [
-          ./configuration.nix
-        ];
-      };
-    };  homeConfigurations = {
-    "${settings.username}@desktop" = inputs.home-manager.lib.homeManagerConfiguration {
+  inputs,
+  pkgs,
+  pkgsUnstable,
+  system,
+  defaultSettings,
+  ...
+}: let
+  settings = defaultSettings // {
+    hostname = "desktop";
+  };
+in {
+  nixosConfigurations = {
+    "${settings.hostname}" = inputs.nixpkgs.lib.nixosSystem {
+      inherit system;
+      specialArgs = {inherit inputs settings pkgsUnstable;};
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+  homeConfigurations = {
+    "${settings.username}@${settings.hostname}" = inputs.home-manager.lib.homeManagerConfiguration {
       inherit pkgs;
-      extraSpecialArgs = { inherit inputs settings; };
+      extraSpecialArgs = {inherit inputs settings;};
       modules = [
         ./home.nix
       ];
