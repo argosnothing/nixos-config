7c555d570bc6fa9cb853ca74e5ccfe0ac3b9941c
argos <argosnothing@gmail.com>
2025-08-30 15:23:17 -0400

    moved plasma manager to a module that defaults to off for now

--- nixosModules/user/wms/plasma/default.nix
+++ nixosModules/user/wms/plasma/default.nix
@@ -1,14 +1,10 @@
 {
-  inputs,
   config,
   pkgs,
   lib,
   ...
 }: {
   imports = [
-    inputs.plasma-manager.homeManagerModules.plasma-manager
-    ./panel.nix
-    ./input.nix
   ];
   options = {
     wms.plasma.enable = lib.mkOption {
@@ -26,29 +22,6 @@
       kdePackages.krfb
       easyeffects
     ];
-    programs.plasma = {
-      enable = true;
-      overrideConfig = true;
-      
-      workspace = {
-        lookAndFeel = "org.kde.breezedark.desktop";
-        colorScheme = "BreezeDark";
-        theme = "breeze-dark";
-      };
-      
-      kwin.virtualDesktops = {
-        number = 4;
-        names = [ "Desktop 1" "Desktop 2" "Desktop 3" "Desktop 4" ];
-      };
-      
-      hotkeys.commands = {
-        launch-discord = {
-          name = "Launch Discord";
-          key = "Alt+Q";
-          command = "discord";
-        };
-      };
-    };
     services.kdeconnect.enable = true;
     styles.stylix.enable = false; # less headache just turning this off for a full dektop environment. 
   };
