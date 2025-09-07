6d4e118672cb42d97da8f20fd76c96ee618c63b2
argos <argosnothing@gmail.com>
2025-09-07 14:13:37 -0400

    ok

--- outputs/default.nix
+++ outputs/default.nix
@@ -1,6 +1,20 @@
-{
+{inputs, ...}: let
+  system = "x86_64-linux";
+  pkgs = import inputs.nixpkgs {
+    inherit system;
+    config.allowUnfree = true;
+  };
+  pkgsUnstable = import inputs.nixpkgs-unstable {
+    inherit system;
+    config.allowUnfree = true;
+  };
+in {
   imports = [
     ./nixos-configs.nix
     ./home-configs.nix
   ];
+
+  _module.args = {
+    inherit pkgs pkgsUnstable;
+  };
 }
