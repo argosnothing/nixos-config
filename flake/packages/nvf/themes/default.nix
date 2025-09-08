9aa2aaa9c4cc9843fef18b4e42ae3283b0e99fb3
argos <argosnothing@gmail.com>
2025-09-07 22:18:10 -0400

    a bunch of stuff probably

--- flake/packages/nvf/themes/default.nix
+++ flake/packages/nvf/themes/default.nix
@@ -10,10 +10,19 @@
 in {
   vim = {
     startPlugins = with pkgs.vimPlugins; [
+      everforest
+      iceberg-vim
     ];
 
-    extraPlugins = with pkgs.vimPlugins; {
-      catppuccin = createTheme "catppuccin" catppuccin-nvim;
-    };
+    extraPlugins = with pkgs.vimPlugins;
+      {
+        kanagawa = createTheme "kanagawa" kanagawa-nvim;
+        ayu = createTheme "ayu" neovim-ayu;
+        nightfox = createTheme "nightfox" nightfox-nvim;
+        rose-pine = createTheme "rose-pine" rose-pine;
+        catppuccin = createTheme "catppuccin" catppuccin-nvim;
+        gruvbox = createTheme "gruvbox" gruvbox-nvim;
+        starrynight = basicTheme starrynight;
+      };
   };
 }
