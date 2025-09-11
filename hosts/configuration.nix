0b275c036608ec96b56540c988da6f27f3a30c18
argos <argosnothing@gmail.com>
2025-09-11 15:13:37 -0400

    fsefse

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -13,6 +13,7 @@
   sops.defaultSopsFile = ../secrets/secrets.yaml;
   sops.defaultSopsFormat = "yaml";
   sops.age.keyFile = "/home/${settings.username}/.config/sops/age/keys.txt";
+  sops.secrets.ssh = {};
   sops.secrets.example-key = {};
   sops.secrets."myservice/my_subdir/my_secret" = {};
   # Shared system configuration for all hosts
