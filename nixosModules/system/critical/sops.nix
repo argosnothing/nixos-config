8897dfb24df621033f7469838d7560d14e43f444
salivala <argosnothing@gmail.com>
2025-09-12 16:50:32 -0400

    no words

--- nixosModules/system/critical/sops.nix
+++ nixosModules/system/critical/sops.nix
@@ -4,6 +4,11 @@
   sops.age.keyFile = "/home/${settings.username}/.config/sops/age/keys.txt";
   sops.secrets.ssh = {};
   sops.secrets.example-key = {};
-  sops.secrets."pc_password" = {};
+  sops.secrets.pc_password = {
+    owner = "root";
+    group = "root";
+    mode = "0400";
+    neededForUsers=true;
+  };
   sops.secrets."myservice/my_subdir/my_secret" = {};
 }
