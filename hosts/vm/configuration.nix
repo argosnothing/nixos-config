94a5c10d346af2375b56a6fcdad8dd156f75712e
salivala <argosnothing@gmail.com>
2025-09-12 10:12:26 -0400

    tweak

--- hosts/vm/configuration.nix
+++ hosts/vm/configuration.nix
@@ -1,12 +1,12 @@
 {pkgs, settings, ...}: {
   imports = [];
+  environment.systemPackages = with pkgs; [git vim];
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.supportedFilesystems = ["zfs"];
   boot.initrd.supportedFilesystems = ["zfs"];
   boot.zfs.devNodes = "/dev/disk/by-partuuid";
   networking.hostId = "openssl rand -hex 4"; # put contents of that command here
-  environment.systemPackages = with pkgs; [git vim];
 
   services.zfs.autoScrub.enable = true;
   services.zfs.trim.enable = true;
