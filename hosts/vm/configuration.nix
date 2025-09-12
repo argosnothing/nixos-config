dfb8b4469f7b70ab1100a728558c3c74a5c993ec
salivala <argosnothing@gmail.com>
2025-09-12 09:58:37 -0400

    created test vm output

--- hosts/vm/configuration.nix
+++ hosts/vm/configuration.nix
@@ -1,3 +1,12 @@
 {pkgs, settings, ...}: {
   imports = [];
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.supportedFilesystems = ["zfs"];
+  boot.initrd.supportedFilesystems = ["zfs"];
+  boot.zfs.devNodes = "/dev/disk/by-partuuid";
+  networking.hostId = "openssl rand -hex 4"; # put contents of that command here
+
+  services.zfs.autoScrub.enable = true;
+  services.zfs.trim.enable = true;
 }
