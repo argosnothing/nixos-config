31962003b7cd1ab2de2fa35f8278a5d569bfc869
salivala <argosnothing@gmail.com>
2025-09-12 14:04:28 -0400

    rebuild time uh oh

--- hosts/vm/attrs.nix
+++ hosts/vm/attrs.nix
@@ -1,6 +1,10 @@
-{
+rec {
+  isvm = true;
   boot = {
     firmware = "uefi";
+  };
+  zfs = {
     hostId = "deadbeef";
+    devNodes = "/dev/disk/" + (if isvm then "by-partuuid" else "by-id");
   };
 }
--- nixosModules/system/critical/zfs.nix
+++ nixosModules/system/critical/zfs.nix
@@ -1,2 +1,18 @@
-{pkgs, config, settings, lib, ...}: {
+{
+  config,
+  settings,
+  lib,
+  ...
+}: {
+  options = {
+    zfs.enable = lib.mkEnableOption "Enable Zfs";
+  };
+  config = lib.mkIf config.zfs.enable {
+    boot.supportedFilesystems = ["zfs"];
+    boot.initrd.supportedFilesystems = ["zfs"];
+    boot.zfs.devNodes = settings.zfs.devNodes;
+    networking.hostId = settings.zfs.hostId;
+    services.zfs.autoScrub.enable = true;
+    services.zfs.trim.enable = true;
+  };
 }
