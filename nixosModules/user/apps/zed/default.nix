ee2416435038651ed70df11b75bea0783bf014a7
salivala <argosnothing@gmail.com>
2025-09-16 18:41:52 -0400

    Generation: 122 nixpkgs: 25.11.20250913.c23193b Kernel: 6.12.47

--- nixosModules/user/apps/zed/default.nix
+++ nixosModules/user/apps/zed/default.nix
@@ -10,10 +10,11 @@
   config = lib.mkIf config.zed.enable {
     programs.zed-editor = {
       enable = true;
-      #extensions = ["nix" "toml" "elixir" "make" "rust"];
+      extensions = ["nix" "toml" "elixir" "make" "rust"];
       userSettings = {
         vim_mode = true;
       };
     };
+    custom.home.persist.directories = [".config/zed"];
   };
 }
