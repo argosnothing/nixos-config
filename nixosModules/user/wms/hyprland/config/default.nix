190155d4496e4fc550b0e4ed36ceebd615b3c41e
salivala <argosnothing@gmail.com>
2025-09-17 10:49:47 -0400

    Generation: 129 nixpkgs: 25.11.20250913.c23193b Kernel: 6.12.47

--- nixosModules/user/wms/hyprland/config/default.nix
+++ nixosModules/user/wms/hyprland/config/default.nix
@@ -1,7 +1,17 @@
-{...}: {
+{pkgs, ...}: {
   imports = [
     ./cursor.nix
     ./nav-bindings.nix
     ./packages.nix
   ];
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = [pkgs.xdg-desktop-portal-gtk];
+    config = {
+      common = {
+        "org.freedesktop.portal.FileChooser" = ["gtk"];
+      };
+    };
+  };
 }
