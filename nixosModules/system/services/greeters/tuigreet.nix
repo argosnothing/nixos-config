3793cc019fa2714f6c8d59177eb2233506b76091
salivala <argosnothing@gmail.com>
2025-09-19 12:35:13 -0400

    Generation: 191 nixpkgs: 25.11.20250913.c23193b Kernel: 6.12.47

--- nixosModules/system/services/greeters/tuigreet.nix
+++ nixosModules/system/services/greeters/tuigreet.nix
@@ -23,7 +23,7 @@
       enable = true;
       settings = {
         default_session = {
-          command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd ${config.greeters.tuigreet.wm}";
+          command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd ${config.custom.greeters.tuigreet.run-command}";
           user = "greeter";
         };
       };
