04268628aab0749197db2231b0d48ea552486fee
salivala <argosnothing@gmail.com>
2025-09-18 21:45:43 -0400

    Generation: 187 nixpkgs: 25.11.20250913.c23193b Kernel: 6.12.47

--- nixosModules/user/apps/nvf/keymaps.nix
+++ nixosModules/user/apps/nvf/keymaps.nix
@@ -10,6 +10,11 @@
         mode = "n";
         action = ":ToggleTerm dir=%:p:h<CR>";
       }
+      {
+        key = "<C-o>";
+        mode = "n";
+        action = ":Oil";
+      }
     ];
   };
 }
