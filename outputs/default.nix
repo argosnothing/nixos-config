a2614de502fb98aba613c427ab984dde62a0de77
salivala <argosnothing@gmail.com>
2025-09-19 12:28:53 -0400

    Generation: 191 nixpkgs: 25.11.20250913.c23193b Kernel: 6.12.47

--- outputs/default.nix
+++ outputs/default.nix
@@ -1,17 +1,7 @@
-{inputs, ...}: let
-  system = "x86_64-linux";
-  pkgs = import inputs.nixpkgs-unstable {
-    inherit system;
-    config.allowUnfree = true;
-  };
-in {
+{...}: {
   imports = [
     ./nixos-configs.nix
     ./packages.nix
     ./dev-shells.nix
   ];
-
-  _module.args = {
-    inherit pkgs;
-  };
 }
