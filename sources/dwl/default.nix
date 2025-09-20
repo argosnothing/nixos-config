977fa234e98d0ee2d5a898403203ddfd06797bbf
salivala <argosnothing@gmail.com>
2025-09-20 16:08:06 -0400

    Generation: 211 nixpkgs: 25.11.20250913.c23193b Kernel: 6.12.47

--- sources/dwl/default.nix
+++ sources/dwl/default.nix
@@ -1,1 +1,26 @@
-
+{pkgs ? import <nixpkgs> {}}:
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    clang
+    clang-tools
+    lldb
+    gdb
+    pkg-config
+    meson
+    ninja
+  ];
+  buildInputs = with pkgs; [
+    libinput
+    pixman
+    xorg.libX11
+    wayland
+    wayland-protocols
+    wayland-scanner
+    wlroots_0_18
+    wlroots_0_17
+    wlroots_0_19
+    libxkbcommon
+  ];
+  packages = with pkgs; [
+  ];
+}
