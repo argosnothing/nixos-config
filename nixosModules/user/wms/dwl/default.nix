e9d9c0374153ed7de4e1bf0c9c5135daac665ba5
salivala <argosnothing@gmail.com>
2025-09-20 13:22:15 -0400

    Generation: 206 nixpkgs: 25.11.20250913.c23193b Kernel: 6.12.47

--- nixosModules/user/wms/dwl/default.nix
+++ nixosModules/user/wms/dwl/default.nix
@@ -8,6 +8,7 @@
     wms.dwl.enable = lib.mkEnableOption "Enable User DWL";
   };
   config = lib.mkIf (config.custom.wm.name == "dwl") {
+    styles.stylix.enable = true;
     home.packages = with pkgs; [
     ];
   };
