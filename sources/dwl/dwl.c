4255a8f1945e072b1ae69609d8f6eeb2d49801c4
salivala <argosnothing@gmail.com>
2025-09-20 19:23:25 -0400

    Generation: 235 nixpkgs: 25.11.20250913.c23193b Kernel: 6.12.47

--- sources/dwl/dwl.c
+++ sources/dwl/dwl.c
@@ -2095,7 +2095,7 @@ pointerfocus(Client *c, struct wlr_surface *surface, double sx, double sy,
 
 void
 scenebuffersetopacity(struct wlr_scene_buffer *buffer, int sx, int sy, void *data) {
-  Client *c = data;
+  //Client *c = data;
   //wlr_scene_buffer_set_opacity(buffer, c->opacity);
 }
 
