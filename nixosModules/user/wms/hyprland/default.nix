d0285422c0b97748f60ef75a1e8d8e6ff1ebd3a2
salivala <argosnothing@gmail.com>
2025-09-21 18:30:35 -0400

    Generation: 266 nixpkgs: 25.11.20250919.8eaee11 Kernel: 6.12.47

--- nixosModules/user/wms/hyprland/default.nix
+++ nixosModules/user/wms/hyprland/default.nix
@@ -18,7 +18,7 @@ in {
     };
   };
   config = lib.mkIf (config.custom.wm.name == "hyprland") {
-    custom.desktop-shell.name = "caelestia-shell";
+    custom.desktop-shell.name = "noctalia-shell";
     wms.hyprland.enable = true;
     styles.stylix.enable = true;
     custom.persist.home.files = ["${hyprlandLocal}/lastNag" "${hyprlandLocal}/lastVersion"];
