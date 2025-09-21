c6503d2e912f90b97ae4661b1e251d89f6d3686b
salivala <argosnothing@gmail.com>
2025-09-21 18:20:32 -0400

    Generation: 264 nixpkgs: 25.11.20250919.8eaee11 Kernel: 6.12.47

--- nixosModules/user/ricing/shells/dank-shell.nix
+++ nixosModules/user/ricing/shells/dank-shell.nix
@@ -7,12 +7,14 @@
 }: let
   dankShell = inputs.dank-shell;
 in {
+  import = [
+    dankShell.homeModules.dankMaterialShell
+  ];
   config = lib.mkIf (config.custom.desktop-shell.name == "dank-shell") {
     home.packages = [
       dankShell.packages.${pkgs.system}.quickshell
       dankShell.packages.${pkgs.system}.dankMaterialShell
       dankShell.packages.${pkgs.system}.default
-      dankShell.homeModules.dankMaterialShell
     ];
     custom.desktop-shell = {
       execCommand = "dms run";
