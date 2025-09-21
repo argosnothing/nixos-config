15f4613bb8a925814c4f6107507bb5174f03a1b7
salivala <argosnothing@gmail.com>
2025-09-21 18:18:57 -0400

    Generation: 264 nixpkgs: 25.11.20250919.8eaee11 Kernel: 6.12.47

--- nixosModules/user/ricing/shells/dank-shell.nix
+++ nixosModules/user/ricing/shells/dank-shell.nix
@@ -12,7 +12,7 @@ in {
       dankShell.packages.${pkgs.system}.quickshell
       dankShell.packages.${pkgs.system}.dankMaterialShell
       dankShell.packages.${pkgs.system}.default
-      dankShell.homeModules.dankMaterialShell.default
+      dankShell.homeModules.${pkgs.system}.dankMaterialShell
     ];
     custom.desktop-shell = {
       execCommand = "dms run";
