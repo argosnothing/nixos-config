67622988c1fe648438f3317d11b8800b2dfe7949
salivala <argosnothing@gmail.com>
2025-09-23 19:07:22 -0400

    Generation: 409 nixpkgs: 25.11.20250921.554be64 Kernel: 6.12.48

--- nixosModules/user/wms/mango/default.nix
+++ nixosModules/user/wms/mango/default.nix
@@ -45,19 +45,45 @@ in {
           bg = mkLiteral c.base00;
           fg = mkLiteral c.base05;
           accent = mkLiteral c.base0E;
+          font = mkLiteral "FiraCode Nerd Font 20";
         };
-        window = {background-color = mkLiteral "@bg";};
+
+        window = {
+          "background-color" = mkLiteral "@bg";
+          border = 0;
+        };
+        mainbox = {
+          "background-color" = mkLiteral "@bg";
+          padding = mkLiteral "8px";
+        };
         inputbar = {
-          background-color = mkLiteral "@bg";
+          "background-color" = mkLiteral "@bg";
+          "text-color" = mkLiteral "@fg";
+          padding = mkLiteral "10px 14px";
+        };
+        prompt = {
+          "background-color" = mkLiteral "@bg";
+          "text-color" = mkLiteral "@fg";
+        };
+        entry = {
+          "background-color" = mkLiteral "@bg";
+          "text-color" = mkLiteral "@fg";
+        };
+
+        listview = {
+          "background-color" = mkLiteral "@bg";
+          lines = 12;
+        };
+        element = {
+          "background-color" = mkLiteral "@bg";
+          "text-color" = mkLiteral "@fg";
           padding = mkLiteral "8px 12px";
         };
-        listview = {lines = 12;};
-        element = {padding = mkLiteral "8px 12px";};
         "element-icon" = {size = mkLiteral "1.3em";};
-        "element-text" = {text-color = mkLiteral "@fg";};
+        "element-text" = {"text-color" = mkLiteral "@fg";};
         "element selected" = {
-          background-color = mkLiteral "@accent";
-          text-color = mkLiteral "@bg";
+          "background-color" = mkLiteral "@accent";
+          "text-color" = mkLiteral "@bg";
         };
       };
     };
