1c645e085202657522c3e9590dd8eff8eaf467e5
salivala <argosnothing@gmail.com>
2025-09-23 09:17:06 -0400

    Generation: 307 nixpkgs: 25.11.20250919.8eaee11 Kernel: 6.12.47

--- nixosModules/user/wms/mango/scratchpads.nix
+++ nixosModules/user/wms/mango/scratchpads.nix
@@ -1,4 +1,6 @@
 ''
   bind=ALT,z,toggle_scratchpad
   bind=SUPER,i,minimized,
+  bind=alt,q,toggle_named_scratchpad,discord,none,discord
+  windowrule=isnamedscratchpad:1,width:1280,height:800,appid:discord
 ''
