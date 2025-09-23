c209478aff33464317a006371277b115edff63ba
salivala <argosnothing@gmail.com>
2025-09-23 18:53:28 -0400

    Generation: 406 nixpkgs: 25.11.20250921.554be64 Kernel: 6.12.48

--- nixosModules/user/wms/mango/default.nix
+++ nixosModules/user/wms/mango/default.nix
@@ -1,3 +1,4 @@
+# This file is going to be a mess for a bit as i configure mango
 {
   pkgs,
   config,
@@ -15,6 +16,16 @@
   styling = import ./styling.nix {inherit config;};
   tags = import ./tags.nix;
   misc = import ./misc.nix;
+  c = config.lib.stylix.colors.withHashtag;
+  rofi-theme = ''
+    * { bg: ${c.base00}; fg: ${c.base05}; accent: ${c.base0E}; }
+    window { background-color: @bg; }
+    inputbar { background-color: @bg; padding: 6px 10px; }
+    listview { lines: 12; }
+    element { padding: 8px 12px; }
+    element selected { background-color: @accent; color: @bg; }
+    element-icon { size: 1.2em; }
+  '';
 in {
   imports = [
     ./config
@@ -28,7 +39,17 @@ in {
     styles.stylix.enable = true;
     stylix.targets.rofi.enable = true;
     services.swaync.enable = true;
-    programs.rofi.enable = true;
+    programs.rofi = {
+      enable = true;
+      extraConfig = {
+        font = "FiraCode Nerd Font 18";
+        modi = "drun,run";
+        show-icons = true;
+        icon-theme = "Papirus";
+        drun-display-format = "{name}";
+      };
+      theme = rofi-theme;
+    };
     home.packages = with pkgs; [
       pavucontrol
       swaybg
