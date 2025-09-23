9e48dd216bb192a5fe91f769fe624995a2170b08
salivala <argosnothing@gmail.com>
2025-09-23 19:14:08 -0400

    Generation: 410 nixpkgs: 25.11.20250921.554be64 Kernel: 6.12.48

--- nixosModules/user/wms/mango/default.nix
+++ nixosModules/user/wms/mango/default.nix
@@ -40,50 +40,51 @@ in {
         icon-theme = "Papirus";
         drun-display-format = "{name}";
       };
+
       theme = {
         "*" = {
-          bg = mkLiteral c.base00;
-          fg = mkLiteral c.base05;
+          background = mkLiteral c.base00;
+          foreground = mkLiteral c.base05;
           accent = mkLiteral c.base0E;
           font = mkLiteral "FiraCode Nerd Font 20";
         };
 
         window = {
-          "background-color" = mkLiteral "@bg";
+          "background-color" = mkLiteral "@background";
           border = 0;
         };
         mainbox = {
-          "background-color" = mkLiteral "@bg";
+          "background-color" = mkLiteral "@background";
           padding = mkLiteral "8px";
         };
         inputbar = {
-          "background-color" = mkLiteral "@bg";
-          "text-color" = mkLiteral "@fg";
+          "background-color" = mkLiteral "@background";
+          "text-color" = mkLiteral "@foreground";
           padding = mkLiteral "10px 14px";
         };
         prompt = {
-          "background-color" = mkLiteral "@bg";
-          "text-color" = mkLiteral "@fg";
+          "background-color" = mkLiteral "@background";
+          "text-color" = mkLiteral "@foreground";
         };
         entry = {
-          "background-color" = mkLiteral "@bg";
-          "text-color" = mkLiteral "@fg";
+          "background-color" = mkLiteral "@background";
+          "text-color" = mkLiteral "@foreground";
         };
 
         listview = {
-          "background-color" = mkLiteral "@bg";
+          "background-color" = mkLiteral "@background";
           lines = 12;
         };
         element = {
-          "background-color" = mkLiteral "@bg";
-          "text-color" = mkLiteral "@fg";
+          "background-color" = mkLiteral "@background";
+          "text-color" = mkLiteral "@foreground";
           padding = mkLiteral "8px 12px";
         };
         "element-icon" = {size = mkLiteral "1.3em";};
-        "element-text" = {"text-color" = mkLiteral "@fg";};
+        "element-text" = {"text-color" = mkLiteral "@foreground";};
         "element selected" = {
           "background-color" = mkLiteral "@accent";
-          "text-color" = mkLiteral "@bg";
+          "text-color" = mkLiteral "@background";
         };
       };
     };
