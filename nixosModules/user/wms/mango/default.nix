50af9f29b614021842aee77b528e4fc0a82eec93
salivala <argosnothing@gmail.com>
2025-09-22 21:31:43 -0400

    Generation: 289 nixpkgs: 25.11.20250919.8eaee11 Kernel: 6.12.47

--- nixosModules/user/wms/mango/default.nix
+++ nixosModules/user/wms/mango/default.nix
@@ -17,7 +17,7 @@
         # More option see https://github.com/DreamMaoMao/mango/wiki/
 
         monitorrule=DP-1,0.55,1,tile,0,1,0,0,3840,2160,144
-        monitorrule=DP-2,0.55,1,tile,0,1,3072,288,1920,1080,60
+        monitorrule=DP-2,0.55,1,tile,0,1,3840,288,1920,1080,60
         # Window effect
         blur=0
         blur_layer=0
