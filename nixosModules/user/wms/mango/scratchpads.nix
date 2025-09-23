98773010d9530418117942e992290abd0ae33284
salivala <argosnothing@gmail.com>
2025-09-23 13:19:20 -0400

    Generation: 340 nixpkgs: 25.11.20250921.554be64 Kernel: 6.12.48

--- nixosModules/user/wms/mango/scratchpads.nix
+++ nixosModules/user/wms/mango/scratchpads.nix
@@ -9,6 +9,6 @@
 
 
   # WEB
-  bind=alt,w,toggle_named_scratchpad,firefox --name=firefox-scratchpad --no-remote -P firefox-scratchpad,none,firefox-scratchpad
+  bind=alt,w,toggle_named_scratchpad,firefox-scratchpad,none,firefox --name=firefox-scratchpad --no-remote -P firefox-scratchpad
   windowrule=isnamedscratchpad:1,width:1280,height:800,appid:firefox-scratchpad
 ''
