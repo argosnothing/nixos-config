62bf1404ca78f5d0d378eed0899e4b17b35c33ac
salivala <argosnothing@gmail.com>
2025-09-23 21:51:04 -0400

    Generation: 446 nixpkgs: 25.11.20250921.554be64 Kernel: 6.12.48

--- nixosModules/user/wms/mango/scratchpads.nix
+++ nixosModules/user/wms/mango/scratchpads.nix
@@ -11,6 +11,10 @@
   bind=alt,w,toggle_named_scratchpad,firefox-scratchpad,none,firefox --name=firefox-scratchpad --no-remote -P firefox-scratchpad
   windowrule=isnamedscratchpad:1,width:1280,height:800,appid:firefox-scratchpad,overlay:1
 
+  # KITTY
+  bind=alt,e,toggle_named_scratchpad,kitty-scratchpad,none,kitty --app-id kitty-scratchpad
+  windowrule=isnamedscratchpad:1,width:1280,height:800,appid:kitty-scratchpad,overlay:1
+
   # nemo nemo
   windowrule=isfloating:1,width:1280,height:800,appid:nemo
 
