a198c27704750f29c9b18389bc13cca709700bbb
salivala <argosnothing@gmail.com>
2025-09-23 21:10:35 -0400

    Generation: 443 nixpkgs: 25.11.20250921.554be64 Kernel: 6.12.48

--- nixosModules/user/wms/mango/scratchpads.nix
+++ nixosModules/user/wms/mango/scratchpads.nix
@@ -1,11 +1,11 @@
 ''
   # DISCORD
   bind=alt,q,toggle_named_scratchpad,vesktop,none,vesktop
-  windowrule=isnamedscratchpad:1,width:1280,height:800,appid:vesktop
+  windowrule=isnamedscratchpad:1,width:1280,height:800,appid:vesktop,isoverlay:1
 
   # SPOTIFY
   bind=alt,s,toggle_named_scratchpad,spotify,none,spotify
-  windowrule=isnamedscratchpad:1,width:1280,height:800,appid:spotify
+  windowrule=isnamedscratchpad:1,width:1280,height:800,appid:spotify,isoverlay:1
 
   # WEB
   bind=alt,w,toggle_named_scratchpad,firefox-scratchpad,none,firefox --name=firefox-scratchpad --no-remote -P firefox-scratchpad
