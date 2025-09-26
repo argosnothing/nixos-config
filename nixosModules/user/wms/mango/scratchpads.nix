cb9042f8d82ecca89c81b943d35f00597e1965c1
salivala <argosnothing@gmail.com>
2025-09-25 21:14:12 -0400

    Generation: 471 nixpkgs: 25.11.20250924.e643668 Kernel: 6.12.48

--- nixosModules/user/wms/mango/scratchpads.nix
+++ nixosModules/user/wms/mango/scratchpads.nix
@@ -1,6 +1,4 @@
 ''
-  scratchpad_cross_monitor,1
-
   # DISCORD
   bind=alt,q,toggle_named_scratchpad,vesktop,none,vesktop
   windowrule=isnamedscratchpad:1,width:1280,height:800,appid:vesktop,isoverlay:1
