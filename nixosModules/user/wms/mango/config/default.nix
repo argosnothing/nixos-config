880b9bd9bb7c8548275b08f3a11a6b890b0fc16f
salivala <argosnothing@gmail.com>
2025-09-26 21:38:09 -0400

    Generation: 489 nixpkgs: 25.11.20250924.e643668 Kernel: 6.12.48

--- nixosModules/user/wms/mango/config/default.nix
+++ nixosModules/user/wms/mango/config/default.nix
@@ -1,6 +1,7 @@
 {...}: {
   imports = [
     ./waybar
+    ./rofi
     ./scripts.nix
   ];
 }
--- nixosModules/user/wms/mango/default.nix
+++ nixosModules/user/wms/mango/default.nix
@@ -7,18 +7,7 @@
   inputs,
   ...
 }: let
-  binds = import ./binds.nix;
-  monitors = import ./monitors.nix;
-  mango-inputs = import ./inputs.nix;
-  layouts = import ./layouts.nix;
-  overview = import ./overview.nix;
-  scratchpads = import ./scratchpads.nix;
-  styling = import ./styling.nix {inherit config;};
-  tags = import ./tags.nix;
-  misc = import ./misc.nix;
-  c = config.lib.stylix.colors.withHashtag;
-  l = config.lib.formats.rasi.mkLiteral;
-  inherit (config.lib.formats.rasi) mkLiteral;
+  mango-settings = import ./settings {inherit config;};
 in {
   imports = [
     ./config
@@ -32,74 +21,7 @@ in {
     styles.stylix.enable = true;
     stylix.targets.rofi.enable = false;
     services.swaync.enable = true;
-    programs.rofi = {
-      enable = true;
-      extraConfig = {
-        font = "FiraCode Nerd Font 18";
-        modi = "drun,run";
-        show-icons = true;
-        icon-theme = "Papirus";
-        drun-display-format = "{name}";
-      };
-      theme = {
-        "*" = {
-          "background-color" = l c.base00;
-          "foreground-color" = l c.base05;
-          font = "FiraCode Nerd Font 20";
-        };
-
-        window = {
-          "background-color" = l c.base00;
-          border = l "2px";
-          "border-color" = l c.base0E;
-        };
-        mainbox = {
-          "background-color" = l c.base00;
-          padding = l "8px";
-        };
-
-        inputbar = {
-          "background-color" = l c.base00;
-          "text-color" = l c.base05;
-          padding = l "10px 14px";
-        };
-
-        prompt = {"text-color" = l c.base0E;};
-        "case-indicator" = {"text-color" = l c.base0E;};
-
-        listview = {
-          "background-color" = l c.base00;
-          lines = 12;
-          border = l "0px";
-          spacing = l "6px";
-        };
-
-        element = {
-          "background-color" = l c.base00;
-          "text-color" = l c.base05;
-          border = l "0px";
-          padding = l "8px 12px";
-        };
-        "element alternate" = {"background-color" = l c.base00;};
-
-        "element selected" = {
-          "background-color" = l c.base00;
-          "text-color" = l c.base05;
-          border = l "2px";
-          "border-color" = l c.base0E;
-          "border-radius" = l "6px";
-        };
-
-        "element-text" = {
-          "background-color" = l c.base00;
-          "text-color" = l c.base05;
-        };
-        "element-icon" = {
-          "background-color" = l c.base00;
-          size = l "1.3em";
-        };
-      };
-    };
+    custom.desktop-shell.name = "noctalia-shell";
     home.packages = with pkgs; [
       pavucontrol
       swaybg
@@ -123,15 +45,7 @@ in {
           env=QT_AUTO_SCREEN_SCALE_FACTOR,1
           env=QT_WAYLAND_FORCE_DPI,140
         ''
-        + binds
-        + monitors
-        + mango-inputs
-        + layouts
-        + overview
-        + scratchpads
-        + styling
-        + tags
-        + misc;
+        + mango-settings;
     };
   };
 }
