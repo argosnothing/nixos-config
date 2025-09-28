aad3a815bbc32b36f1c8607c8303ed86f353d05e
argosnothing <argosnothing@gmail.com>
2025-09-28 00:23:47 -0400

    Generation: 64 nixpkgs: 25.11.20250924.e643668 Kernel: 6.12.48

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -3,13 +3,6 @@
     ../home.nix # Import shared home configuration
   ];
 
-  services.libinput = {
-    enable = true;
-    touchpad = {
-      accelSpeed = "0.5";
-      accelProfile = "adaptive";
-    };
-  };
   # Add laptop-specific packages to the shared list
   home.packages = with pkgs; [
     loupe
