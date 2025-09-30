593a2e8b3d95ce2a807239b666df323de5212391
salivala <argosnothing@gmail.com>
2025-09-29 20:16:39 -0400

    Generation: 528 nixpkgs: 25.11.20250924.e643668 Kernel: 6.12.48

--- nixosModules/system/apps/utilities/virtualization/default.nix
+++ nixosModules/system/apps/utilities/virtualization/default.nix
@@ -7,5 +7,5 @@
   users.users."${settings.username}".extraGroups = ["libvirtd" "kvm"];
 
   custom.persist.root.directories = ["/var/lib/libvirt"];
-  custom.persist.root.cache.directories = ["/var/cache/libvirt"]
+  custom.persist.root.cache.directories = ["/var/cache/libvirt"];
 }
