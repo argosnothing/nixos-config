c7c520fdcfed0d1dc3470f384e4dbb9a4e53a497
salivala <argosnothing@gmail.com>
2025-10-02 15:57:32 -0400

    last minute changes for desktop. hope i don't regret!

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -1,21 +1,25 @@
 {pkgs, ...}: {
   imports = [
-    ../configuration.nix # Import shared system configuration
+    ../shared/configuration.nix # Import shared system configuration
     ./hardware-configuration.nix
     ./nvidia.nix
   ];
 
-  steam.enable = true;
-  zfs.enable = true;
-  custom.persist.enable = true;
-  via.enable = true;
+  my = {
+    modules = {
+      critical.zfs.enable = true;
+      misc.citrix.enable = true;
+    };
+    persist.enable = true;
+  };
+
   environment.systemPackages = with pkgs; [
     uhk-agent
     cachix
     xorg.xev
     xdg-utils
   ];
-  custom.persist.home.directories = [".config/uhk-agent"];
+  my.persist.home.directories = [".config/uhk-agent"];
   hardware.keyboard.uhk.enable = true;
 
   services.udev.packages = with pkgs; [uhk-agent];
