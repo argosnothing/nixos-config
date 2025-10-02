7e9aafeaf819b222e6f508f160236cc6dbd09e0c
salivala <argosnothing@gmail.com>
2025-10-01 20:26:21 -0400

    files needs to be there obviously

--- modules/gui/wms/mango/config/waybar/default.nix
+++ modules/gui/wms/mango/config/waybar/default.nix
@@ -14,96 +14,98 @@ in {
         recording-widget
         pkgs.waybar
       ];
-      ".config/waybar/style.css".text = waybar-css;
-      ".config/waybar/config.jsonc" = {
-        generator = lib.generators.toJSON {};
-        value = {
-          layer = "top";
-          position = "top";
-          height = 25;
+      files = {
+        ".config/waybar/style.css".text = waybar-css;
+        ".config/waybar/config.jsonc" = {
+          generator = lib.generators.toJSON {};
+          value = {
+            layer = "top";
+            position = "top";
+            height = 25;
 
-          modules-left = ["ext/workspaces" "dwl/window"];
-          modules-center = ["custom/recorder"];
-          modules-right = [
-            "clock"
-            "custom/space"
-            "pulseaudio"
-            "custom/space"
-            "custom/notification"
-            "custom/space"
-            "tray"
-            "custom/space"
-            "wlr/taskbar"
-          ];
+            modules-left = ["ext/workspaces" "dwl/window"];
+            modules-center = ["custom/recorder"];
+            modules-right = [
+              "clock"
+              "custom/space"
+              "pulseaudio"
+              "custom/space"
+              "custom/notification"
+              "custom/space"
+              "tray"
+              "custom/space"
+              "wlr/taskbar"
+            ];
 
-          "ext/workspaces" = {
-            "format" = "{icon}";
-            "ignore-hidden" = false;
-            "on-click" = "activate";
-            "sort-by-id" = true;
-            "persistent-workspaces" = {"*" = [1 2 3 4 5 6 7 8 9];};
-          };
+            "ext/workspaces" = {
+              "format" = "{icon}";
+              "ignore-hidden" = false;
+              "on-click" = "activate";
+              "sort-by-id" = true;
+              "persistent-workspaces" = {"*" = [1 2 3 4 5 6 7 8 9];};
+            };
 
-          "custom/recorder" = {
-            format = "{}";
-            exec = "rec-widget status";
-            "exec-on-event" = true;
-            signal = 8;
-            "on-click" = "rec-widget toggle";
-          };
+            "custom/recorder" = {
+              format = "{}";
+              exec = "rec-widget status";
+              "exec-on-event" = true;
+              signal = 8;
+              "on-click" = "rec-widget toggle";
+            };
 
-          "wlr/taskbar" = {
-            format = "{icon}";
-            "icon-size" = 22;
-            "markup" = true;
-            "tooltip-format" = "{title}";
-            "on-click" = "activate";
-            "on-click-right" = "close";
-            "ignore-list" = ["Wofi" "rofi"];
-            "all-outputs" = false;
-          };
+            "wlr/taskbar" = {
+              format = "{icon}";
+              "icon-size" = 22;
+              "markup" = true;
+              "tooltip-format" = "{title}";
+              "on-click" = "activate";
+              "on-click-right" = "close";
+              "ignore-list" = ["Wofi" "rofi"];
+              "all-outputs" = false;
+            };
 
-          "dwl/layout" = {
-            format = "{}";
-            "max-length" = 20;
-          };
+            "dwl/layout" = {
+              format = "{}";
+              "max-length" = 20;
+            };
 
-          "dwl/window" = {
-            "format" = "[{layout}]{title} | {app_id}";
-          };
+            "dwl/window" = {
+              "format" = "[{layout}]{title} | {app_id}";
+            };
 
-          "custom/space" = {
-            format = "   "; # three spaces
-            interval = "once";
-          };
+            "custom/space" = {
+              format = "   "; # three spaces
+              interval = "once";
+            };
 
-          pulseaudio = {
-            "disable-scroll" = true;
-            "format" = "{icon} {volume}%";
-            "format-muted" = "muted";
-            "on-click" = "pavucontrol";
-            "format-icons".default = ["" "" ""];
-          };
+            pulseaudio = {
+              "disable-scroll" = true;
+              "format" = "{icon} {volume}%";
+              "format-muted" = "muted";
+              "on-click" = "pavucontrol";
+              "format-icons".default = ["" "" ""];
+            };
 
-          tray = {
-            "icon-size" = 20;
-            spacing = 6;
-          };
+            tray = {
+              "icon-size" = 20;
+              spacing = 6;
+            };
 
-          clock = {
-            format = "{:%I:%M %p  %m/%d/%Y}";
-            "tooltip-format" = "<big>{:%A, %B %d, %Y}</big>";
-          };
+            clock = {
+              format = "{:%I:%M %p  %m/%d/%Y}";
+              "tooltip-format" = "<big>{:%A, %B %d, %Y}</big>";
+            };
 
-          "custom/notification" = {
-            format = " {text}";
-            "return-type" = "json";
-            "exec-if" = "which swaync-client";
-            exec = "swaync-client -swb";
-            "on-click" = "swaync-client -t -sw";
-            "on-click-right" = "swaync-client -d -sw";
-            escape = true;
-            interval = 0;
+            "custom/notification" = {
+              format = " {text}";
+              "return-type" = "json";
+              "exec-if" = "which swaync-client";
+              exec = "swaync-client -swb";
+              "on-click" = "swaync-client -t -sw";
+              "on-click-right" = "swaync-client -d -sw";
+              escape = true;
+              interval = 0;
+            };
           };
         };
       };
