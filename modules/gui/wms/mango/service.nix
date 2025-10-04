cd60524a7b226c76ae0dbae5ed355fef66e1fd7a
salivala <argosnothing@gmail.com>
2025-10-04 19:06:48 -0400

    credits

--- modules/gui/wms/mango/service.nix
+++ modules/gui/wms/mango/service.nix
@@ -10,7 +10,21 @@
   inherit (pkgs) system;
   mango-package = inputs.mango.packages.${system}.default;
 in {
-  config =
-    mkIf mango.enable {
+  config = mkIf mango.enable {
+    # CREDIT: 
+    # https://github.com/iynaix/dotfiles/blob/a494fec53ac1441ea78a9014564eb24f4724a285/modules/gui/mango/default.nix#L4
+    systemd.user.targets.mango-session = {
+      unitConfig = {
+        Description = "mango compositor session";
+        Documentation = ["man:systemd.special(7)"];
+        BindsTo = ["graphical-session.target"];
+        Wants = [
+          "graphical-session-pre.target"
+        ];
+        # ++ lib.optional cfg.systemd.xdgAutostart "xdg-desktop-autostart.target";
+        After = ["graphical-session-pre.target"];
+        # Before = lib.optional cfg.systemd.xdgAutostart "xdg-desktop-autostart.target";
+      };
     };
+  };
 }
