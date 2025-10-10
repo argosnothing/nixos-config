483de4e229eea534e11edfd1ad72e7a7663a94af
salivala <argosnothing@gmail.com>
2025-10-10 12:02:35 -0400

    tweaks to vm stuf

--- hosts/vm/configuration.nix
+++ hosts/vm/configuration.nix
@@ -3,6 +3,26 @@
     ../shared/configuration.nix
     ./hardware-configuration.nix
   ];
-  my.modules.critical.zfs.enable = true;
-  my.persist.enable = true;
+  my = {
+    persist.enable = true;
+    modules = {
+      critical.zfs.enable = true;
+      monitors = [
+        {
+          name = "Virtual-1";
+          is-primary = true;
+          dimensions = {
+            width = 1920;
+            height = 1080;
+          };
+          position = {
+            x = 0;
+            y = 0;
+          };
+          scale = 1.0;
+          refresh = 60.0;
+        }
+      ];
+    };
+  };
 }
