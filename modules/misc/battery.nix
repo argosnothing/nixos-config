89f2d72c02907dcc459f0ac711a771564cac6148
salivala <argosnothing@gmail.com>
2025-10-11 10:36:13 -0400

    made battery stuff work

--- modules/misc/battery.nix
+++ modules/misc/battery.nix
@@ -12,17 +12,8 @@ in {
   config = mkIf enable {
     powerManagement.powertop.enable = true;
     services = {
-      power-profiles-daemon.enable = false;
-      tlp = {
-        enable = true;
-        settings = {
-          CPU_BOOST_ON_AC = 1;
-          CPU_BOOST_ON_BAT = 0;
-          CPU_SCALING_GOVERNOR_ON_AC = "performance";
-          CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-          STOP_CHARGE_THRESH_BAT0 = 95;
-        };
-      };
+      upower.enable = true;
+      power-profiles-daemon.enable = true;
     };
   };
 }
