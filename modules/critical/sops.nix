9cdc791ecdaf9a8d2aa000acbeb1901a3891d66b
salivala <argosnothing@gmail.com>
2025-10-23 16:47:43 -0400

    weeee

--- modules/critical/sops.nix
+++ modules/critical/sops.nix
@@ -5,35 +5,41 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  flake.modules.nixos.critical = {config, self, ...}: {
-    sops = {
-      defaultSopsFile = ../../secrets/secrets.yaml;
-      defaultSopsFormat = "yaml";
-      age.keyFile =
-        if config.impermanence.enable
-        then "/persist/home/${config.user.name}/.config/sops/age/keys.txt"
-        else "/home/${config.user.name}/.config/sops/age/keys.txt";
-      secrets = {
-        ssh = {
-          sopsFile = ../../secrets/secrets.yaml;
-          path = "/home/${config.user.name}/.ssh/id_ed25519";
-          owner = "${config.user.name}";
-          mode = "0600";
+  flake.modules.nixos.critical = {
+    config,
+    self,
+    ...
+  }: {
+    config = {
+      sops = {
+        defaultSopsFile = ../../secrets/secrets.yaml;
+        defaultSopsFormat = "yaml";
+        age.keyFile =
+          if config.impermanence.enable
+          then "/persist/home/${config.user.name}/.config/sops/age/keys.txt"
+          else "/home/${config.user.name}/.config/sops/age/keys.txt";
+        secrets = {
+          ssh = {
+            sopsFile = ../../secrets/secrets.yaml;
+            path = "/home/${config.user.name}/.ssh/id_ed25519";
+            owner = "${config.user.name}";
+            mode = "0600";
+          };
+          example-key = {};
+          pc_password = {
+            owner = "root";
+            group = "root";
+            mode = "0400";
+            neededForUsers = true;
+          };
+          "zwift_credentials/email" = {};
+          "zwift_credentials/password" = {};
+          "myservice/my_subdir/my_secret" = {};
         };
-        example-key = {};
-        pc_password = {
-          owner = "root";
-          group = "root";
-          mode = "0400";
-          neededForUsers = true;
-        };
-        "zwift_credentials/email" = {};
-        "zwift_credentials/password" = {};
-        "myservice/my_subdir/my_secret" = {};
       };
-    };
-    environment.shellAliases = {
-      secrets = "sops ${self}/secrets/secrets.yaml";
+      environment.shellAliases = {
+        secrets = "sops ${self}/secrets/secrets.yaml";
+      };
     };
   };
 }
--- modules/imports.nix
+++ modules/imports.nix
@@ -1,6 +1,7 @@
 {inputs, ...}: {
   imports = [
     inputs.flake-parts.flakeModules.modules
+
   ];
   flake.modules.nixos.imports = [
     inputs.sops-nix.nixosModules.sops
