09a8bf249a780968788c51867edc62fc62781062
salivala <argosnothing@gmail.com>
2025-10-26 14:25:12 -0400

    added work stuff

---
+++ modules/features/work/citrix.nix
@@ -0,0 +1,34 @@
+{
+  flake.modules.nixos.work = {
+    pkgs,
+    pkgs-stable,
+    ...
+  }: let
+    extraCerts = [
+      ./secure/citrix-certs/Entrust_Root_G2.pem
+      ./secure/citrix-certs/Entrust_L1K.pem
+      ./secure/citrix-certs/dod-pki-bundle.pem
+      ./secure/citrix-certs/DoD_PKE_CA_chain.pem
+      ./secure/citrix-certs/Iden.pem
+    ];
+  in {
+    security.pki.certificateFiles = extraCerts;
+    services.pcscd.enable = true;
+    environment.systemPackages = [
+      pkgs.opensc
+      pkgs.pcsc-tools
+      pkgs.p11-kit
+      (pkgs-stable.citrix_workspace.override {inherit extraCerts;})
+    ];
+
+    my.persist.home = {
+      directories = [
+        ".ICAClient"
+        ".config/Citrix"
+        ".local/share/Citrix"
+        ".pki"
+      ];
+      files = [".local/share/applications/wfica.desktop"];
+    };
+  };
+}
