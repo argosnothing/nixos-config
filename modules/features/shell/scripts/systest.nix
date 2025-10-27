08b5a1303525572faddca24268fc839403f0ca15
salivala <argosnothing@gmail.com>
2025-10-27 16:07:47 -0400

    added systest.nix

---
+++ modules/features/shell/scripts/systest.nix
@@ -0,0 +1,15 @@
+{
+  flake.modules.nixos.misc-scripts = {pkgs, ...}: {
+    environment.systemPacakges = [
+      (pkgs.writeShellScriptBin "systest" ''
+        set -euo pipefail
+        if [ $# -lt 1 ]; then
+          echo "usage: systest <hostname> [build args...]" >&2
+          exit 1
+        fi
+        host="$1"; shift
+        nix build ".#nixosConfigurations.''${host}.config.system.build.toplevel" "$@"
+      '')
+    ];
+  };
+}
