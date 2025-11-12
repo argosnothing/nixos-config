2314795a89ab0cc1cfbd4737ba124c2507cb6d5b
salivala <argosnothing@gmail.com>
2025-11-12 18:45:35 -0500

    krita wins!!!

--- modules/features/gui/apps/krita.nix
+++ modules/features/gui/apps/krita.nix
@@ -1,9 +1,10 @@
 {
-  flake.modules.nixos.krita = {
-    pkgs,
-    lib,
-    ...
-  }: {
+  flake.modules.nixos.krita = {pkgs, ...}: {
     environment.systemPackages = with pkgs; [krita];
+    my.persist = {
+      home.directories = [
+        ".local/share/krita"
+      ];
+    };
   };
 }
