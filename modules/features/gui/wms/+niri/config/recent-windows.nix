bc19ce1a976f7dfb569938bf3968511f3f64fd96
salivala <argosnothing@gmail.com>
2025-11-18 20:41:19 -0500

    stuff

--- modules/features/gui/wms/+niri/config/recent-windows.nix
+++ modules/features/gui/wms/+niri/config/recent-windows.nix
@@ -3,10 +3,12 @@
     my.wm.niri.settings = lib.mkAfter [
       ''
         recent-windows {
-          Mod+Tab         { next-window; }
-          Mod+Shift+Tab   { previous-window; }
-          Mod+grave       { next-window     filter="app-id"; }
-          Mod+Shift+grave { previous-window filter="app-id"; }
+          binds {
+            Mod+Tab         { next-window; }
+            Mod+Shift+Tab   { previous-window; }
+            Mod+grave       { next-window     filter="app-id"; }
+            Mod+Shift+grave { previous-window filter="app-id"; }
+          }
         }
       ''
     ];
