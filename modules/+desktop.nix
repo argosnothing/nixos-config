2b1b3efae697f886ed406873ade1a471129d349e
salivala <argosnothing@gmail.com>
2025-11-20 11:34:17 -0500

    ventoy

--- modules/+desktop.nix
+++ modules/+desktop.nix
@@ -14,6 +14,7 @@
       base
       gui-apps
       shell-apps
+      ventoy
 
       work
       via
---
+++ modules/features/gui/apps/ventoy.nix
@@ -0,0 +1,5 @@
+{
+  flake.modules.nixos.ventoy = {pkgs, ...}: {
+    environment.systemPackages = with pkgs; [ventoy];
+  };
+}
