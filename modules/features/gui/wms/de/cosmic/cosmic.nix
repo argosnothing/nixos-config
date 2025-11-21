334481a854dbc58bd6ae3e577f559d563c668f25
salivala <argosnothing@gmail.com>
2025-11-20 21:30:39 -0500

    tweaks to cosmic

--- modules/features/gui/wms/de/cosmic/cosmic.nix
+++ modules/features/gui/wms/de/cosmic/cosmic.nix
@@ -1,5 +1,11 @@
-{
+{config, ...}: let
+  inherit (config) flake;
+in {
   flake.modules.nixos.cosmic = {
+    imports = with flake.modules.nixos; [
+      cursor
+    ];
+    my.cursor.enable = false;
     my.persist = {
       home.directories = [
         ".config/cosmic"
