b2ee0da69927624359f55d86ebfa4ac51cf50f9b
salivala <argosnothing@gmail.com>
2025-11-20 21:20:38 -0500

    xfce tweaks

--- modules/features/gui/wms/de/xfce/xfce.nix
+++ modules/features/gui/wms/de/xfce/xfce.nix
@@ -1,10 +1,21 @@
 {config, ...}: let
   inherit (config) flake;
 in {
-  flake.modules.nixos.xfce = {
+  flake.modules.nixos.xfce = {pkgs, ...}: {
     imports = with flake.modules.nixos; [
+      cursor
     ];
     my.cursor.enable = false;
+    xdg.portal = {
+      enable = true;
+      extraPortals = with pkgs; [
+        xdg-desktop-portal-gtk
+      ];
+    };
+    environment.systemPackages = with pkgs; [
+      xfce.xfce4-whiskermenu-plugin
+      xfce.xfce4-docklike-plugin
+    ];
     services = {
       xserver = {
         enable = true;
