ccb22f64e22b14f3bbeaa0a75f8175bbc37ae865
salivala <argosnothing@gmail.com>
2025-11-25 12:01:33 -0500

    stole jets config

--- modules/features/gui/apps/editors/vscode.nix
+++ modules/features/gui/apps/editors/vscode.nix
@@ -1,18 +1,15 @@
-{ inputs, ... }:
-{
-  flake.modules.nixos.vscode =
-    { pkgs, ... }:
-    {
-      environment.systemPackages = [
-        inputs.self.packages.${pkgs.stdenv.hostPlatform.system}.vscode
+{inputs, ...}: {
+  flake.modules.nixos.vscode = {pkgs, ...}: {
+    environment.systemPackages = [
+      inputs.self.packages.${pkgs.stdenv.hostPlatform.system}.vscode
+    ];
+    my.persist = {
+      home.directories = [
+        ".config/Code"
+        ".vscode"
+        ".rustup"
+        ".cargo"
       ];
-      my.persist = {
-        home.directories = [
-          ".config/Code"
-          ".vscode"
-          ".rustup"
-          ".cargo"
-        ];
-      };
     };
+  };
 }
