2d26ae0dd91b616f1fadc3dd7fce2c6f33160afb
salivala <argosnothing@gmail.com>
2025-12-01 16:23:24 -0500

    added kernel for laptop

--- modules/hardware/laptop/kernel.nix
+++ modules/hardware/laptop/kernel.nix
@@ -1,5 +1,20 @@
 {
   flake.modules.nixos.laptop = {
+    lib,
+    config,
+    pkgs,
+    ...
+  }: {
     boot.initrd.availableKernelModules = ["xhci_pci" "nvme" "rtsx_pci_sdmmc"];
+    boot.initrd.kernelModules = [];
+    boot.kernelModules = ["kvm-intel"];
+    boot.extraModulePackages = [];
+    swapDevices = [];
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+    hardware = {
+      enableAllFirmware = true;
+      firmware = [pkgs.linux-firmware];
+    };
+    hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   };
 }
