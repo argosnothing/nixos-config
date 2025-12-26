ce57c2d060b4fbf2fbdd24764a5ddb0b8d6509af
salivala <argosnothing@gmail.com>
2025-12-26 10:27:58 -0500

    moving theme stuff into it's own config file.

--- modules/packages/emacs/doom/config/default.el
+++ modules/packages/emacs/doom/config/default.el
@@ -1,3 +1,4 @@
 ;; -*- lexical-binding: t; -*-
 (load! "mcp")
+(load! "theme")
 ;;(load! "lsp")
---
+++ modules/packages/emacs/doom/config/theme.el
@@ -0,0 +1,31 @@
+;;; theme.el --- Themes -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2025
+;;
+;; Author:  <salivala@desktop>
+;; Maintainer:  <salivala@desktop>
+;; Created: December 26, 2025
+;; Modified: December 26, 2025
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex text tools unix vc wp
+;; Homepage: https://github.com/salivala/theme
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Theme stuff i like using in emacs.
+;;
+;;; Code:
+
+
+;;(setq doom-theme 'doom-dark+)
+(setq doom-theme 'doom-ir-black)
+(setq global-hl-line-modes nil)
+;;(add-to-list 'custom-theme-load-path (expand-file-name "~/.config/doom/themes"))
+;;(load-theme 'noctalia t)
+
+
+(provide 'theme)
+;;; theme.el ends here
