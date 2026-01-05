36894971faf083928f22be690708324abf3e9f3d
salivala <argosnothing@gmail.com>
2026-01-04 21:01:59 -0500

    i love zed omg

--- modules/features/gui/wms/+hyprland/config/window-rules.nix
+++ modules/features/gui/wms/+hyprland/config/window-rules.nix
@@ -2,8 +2,6 @@
   flake.modules.nixos.hyprland = {lib, ...}: {
     my.wm.hyprland.settings = lib.mkAfter [
       "windowrule = monitor DP-2, match:title ^Smithay$"
-      #"windowrule = suppressevent maximize, match:class .*"
-      #"windowrule = nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
     ];
   };
 }
