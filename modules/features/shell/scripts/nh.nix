1dade82425fff2d9a313c7bd6f6757dec7f8ead2
argosnothing <argosnothing@gmail.com>
2026-01-11 02:10:31 +0000

    got nh working under wsl

--- modules/features/shell/scripts/nh.nix
+++ modules/features/shell/scripts/nh.nix
@@ -8,11 +8,12 @@ in {
   }: let
     inherit (config.my) hostname;
     actualFlakedir = if hostname == "nixos" then "/home/nixos/nixos-config" else flakedir;
+    actualHostname = if hostname == "nixos" then "wsl" else hostname;
     rebuild = command: ''
       #!/bin/bash
       pushd ${actualFlakedir}
       alejandra . &>/dev/null
-      nh os ${command} ${actualFlakedir}/#nixosConfigurations.${hostname};
+      nh os ${command} ${actualFlakedir}/#nixosConfigurations.${actualHostname};
       popd
     '';
   in {
