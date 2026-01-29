2eec564ad0fe4a82cb81dbc0f74daf1ad7766741
salivala <argosnothing@gmail.com>
2026-01-29 16:51:15 -0500

    readded sessions, this is actually useful

---
+++ modules/options/sessions.nix
@@ -0,0 +1,45 @@
+# Window Managers and desktop environments can register stuff here
+# This is currently unused, but in the future could be useful if i want to
+# generate a display manager menu or something explicitly.
+{
+  flake.modules.nixos.options = {lib, ...}: let
+    inherit (lib) types mkOption;
+    inherit (types) enum listOf str submodule package nullOr;
+  in {
+    options = {
+      my.session = mkOption {
+        type = submodule {
+          options = {
+            name = mkOption {
+              type = str;
+            };
+            exec-command = mkOption {
+              type = str;
+            };
+          };
+        };
+      };
+      # If I ever want to support simultaneous sessions in the future.
+      my.sessions = mkOption {
+        description = "Registered desktop and window manager info";
+        default = [{}];
+        type = listOf (submodule {
+          options = {
+            manage = enum ["desktop" "window"];
+            name = mkOption {
+              type = str;
+            };
+            start = mkOption {
+              description = "startup command for the session";
+              type = str;
+            };
+            package = mkOption {
+              description = "session package";
+              type = nullOr package;
+            };
+          };
+        });
+      };
+    };
+  };
+}
