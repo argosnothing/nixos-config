2256fcc9b7aeaa33dc74f3e887b7ae9cc89e89f4
salivala <argosnothing@gmail.com>
2026-01-30 11:38:57 -0500

    stuff

--- modules/features/gui/wms/startup.nix
+++
@@ -1,9 +0,0 @@
-{lib, ...}: {
-  options.my = {
-    startup-services = lib.mkOption {
-      description = "Services to start after the WM is initialized";
-      type = lib.types.listOf lib.types.str;
-      default = [];
-    };
-  };
-}
