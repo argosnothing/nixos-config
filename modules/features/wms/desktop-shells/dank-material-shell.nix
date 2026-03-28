3c14782a5b627bf59f5fa3d46f59ca090898c04c
salivala <argosnothing@gmail.com>
2026-03-27 21:28:28 -0400

    dms nation

--- modules/features/wms/desktop-shells/dank-material-shell.nix
+++ modules/features/wms/desktop-shells/dank-material-shell.nix
@@ -8,5 +8,15 @@
       enable = true;
       quickshell.package = inputs.quickshell.packages.${pkgs.system}.quickshell;
     };
+    my.persist.home.directories = [
+      ".config/DankMaterialShell"
+      ".local/share/state/DankMaterialShell"
+    ];
+    my.desktop-shells = {
+      enable = true;
+      name = "dms";
+      execCommand = "dms run";
+      launcherCommand = "dms ipc call spotlight toggle";
+    };
   };
 }
