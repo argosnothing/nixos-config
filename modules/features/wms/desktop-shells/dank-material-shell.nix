28ef4f22cec3d5955953449707f95fe94c8550ec
salivala <argosnothing@gmail.com>
2026-03-27 22:03:06 -0400

    added more dms stuff

--- modules/features/wms/desktop-shells/dank-material-shell.nix
+++ modules/features/wms/desktop-shells/dank-material-shell.nix
@@ -11,6 +11,14 @@
     my.persist.home.directories = [
       ".config/DankMaterialShell"
       ".local/share/state/DankMaterialShell"
+      ".local/share/color-schemes"
+      ".local/state/DankMaterialShell"
+      ".config/dgop"
+    ];
+    my.persist.home.cache.directories = [
+      ".cache/DankMaterialShell"
+      ".cache/wal"
+      ".cache/quickshell"
     ];
     my.desktop-shells = {
       enable = true;
