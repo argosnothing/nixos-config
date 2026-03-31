5f0e55771605367ffbb5e281ca3521f5d22c72b0
salivala <argosnothing@gmail.com>
2026-03-31 12:30:28 -0400

    add thing

--- modules/features/wms/+budgie/budgie.nix
+++ modules/features/wms/+budgie/budgie.nix
@@ -1,10 +1,23 @@
 {
   flake.modules.nixos.budgie = {pkgs, ...}: {
+    my.persist.home.directories = [
+      ".config/budgie-desktop"
+      ".config/budgie-control-center"
+      ".config/gammastep"
+      ".local/share/contractor"
+    ];
+    environment.budgie.excludePackages = with pkgs; [
+     mate.mate-terminal
+     gnome-terminal 
+     xterm
+     pluma
+    ];
     services = {
       xserver = {
         enable = true;
         desktopManager.budgie.enable = true;
         displayManager.lightdm.enable = true;
+        excludePackages = with pkgs; [xterm];
       };
     };
   };
