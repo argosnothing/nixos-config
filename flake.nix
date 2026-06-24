e50044e5b90f734957d18402c1be916c23db71a2
salivala <argosnothing@gmail.com>
2026-06-24 11:36:19 -0400

    modernization

--- flake.nix
+++ flake.nix
@@ -62,7 +62,7 @@
       url = "github:NixOS/nixpkgs/46db2e09e1d3f113a13c0d7b81e2f221c63b8ce9";
     };
     nixpkgs-stable = {
-      url = "github:NixOS/nixpkgs/nixos-25.11";
+      url = "github:NixOS/nixpkgs/nixos-26.05";
     };
     nixos-wsl = {
       url = "github:nix-community/nixos-wsl/release-25.05";
